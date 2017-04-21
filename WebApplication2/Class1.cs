c10c62068a4c697f2f34fa625da85843ae448866
psl <pansenlin@aorise.cn>
2017-04-21 09:24:42 +0800

    新建文件Class1

---
+++ WebApplication2/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class Class1
+    {
+        string a = "a";
+        public string FunA(string b)
+        {
+            a = b;
+            return a;
+        }
+
+
+    }
+}
