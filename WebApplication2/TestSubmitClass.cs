e006793a77104dadcbe38df05bac13c9e5eef939
TSX <tanshaoxing@aorise.cn>
2017-04-21 10:22:27 +0800

    新增类 testSubmitClass

---
+++ WebApplication2/TestSubmitClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class TestSubmitClass
+    {
+        public void add() {
+            int a = 3;
+            int b = 4;
+            int sum = a + b;
+        }
+    }
+}
